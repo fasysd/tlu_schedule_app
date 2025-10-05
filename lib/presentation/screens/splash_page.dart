88a5c658d67520565022f6948d15a2466a3a9ec9
fasysd <163738827+fasysd@users.noreply.github.com>
2025-10-05 21:41:12 +0700

    Revert "Revert "Feature/user login""

---
+++ lib/presentation/screens/splash_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 1400), () {
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (_) => const LoginPage()));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset('assets/images/logo.png'),
+              Text('TLU', style: Theme.of(context).textTheme.displayLarge),
+              Text(
+                'HỆ THỐNG QUẢN LÝ LỊCH TRÌNH',
+                style: Theme.of(context).textTheme.displayLarge,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
